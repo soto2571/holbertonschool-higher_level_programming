c8cda0ba009055fd1db5eab0f6a4dc98ddd0ee45
soto2571 <sebastian.soto1649@gmail.com>
2024-05-08 19:10:26 -0400

    Project complete

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10 if number >= 0 else ((number * -1) % 10) * -1
+prefix = "Last digit of"
+str = "and is less than 6 and not 0"
+if last > 5:
+    output = "{} {} is {} and is greater than 5".format(prefix, number, last)
+
+elif last < 6 and last != 0:
+    output = "{} {} is {} {}".format(prefix, number, last, str)
+
+elif last == 0:
+    output = "{} {} is {} and is 0".format(prefix, number, last)
+
+print(output)
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def islower(c):
+    assci_value = ord(c)
+    return 97 <= assci_value <= 122
