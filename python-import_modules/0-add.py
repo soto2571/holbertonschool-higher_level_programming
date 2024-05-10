a018c4e6a2f7a24e04855db3cda243d1fc2d2a2b
soto2571 <sebastian.soto1649@gmail.com>
2024-05-10 07:43:33 -0400

    jsj

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-from add_0 import add
+    from add_0 import add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
 
