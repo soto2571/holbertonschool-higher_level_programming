e415135bfac01a05f0a0e0da60a0d35046416f35
soto2571 <sebastian.soto1649@gmail.com>
2024-05-15 13:18:15 -0400

    fixing coding style

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -7,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-        print("")
-        return count
+        print()
+    return count
