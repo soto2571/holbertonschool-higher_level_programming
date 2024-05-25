d3d6cb87aa2fd9e7580a33de0308e325b45a3cde
soto2571 <sebastian.soto1649@gmail.com>
2024-05-24 21:01:30 -0400

    finishing project

---
+++ python-abc/README.md
@@ -0,0 +1,1 @@
+Another holberton project with python
---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+import math
+
+
+"""
+making a class Shape with two subclasses name Circle, Rectangle
+"""
+
+
+class Shape(ABC):
+    """
+    Abstract base class represents a geometry shape
+
+    Methods:
+        area()
+        perimeter()
+    """
+    @abstractmethod
+    def area(self):
+        """
+        Calculates the area of the shape
+
+        Returns:
+            float: the area of the shape
+        """
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """
+        Calculates the perimeter of the shape
+
+        Returns:
+            float: the perimeter of the shape
+        """
+        pass
+
+
+class Circle(Shape):
+    """
+    Circle class, subclass of Shape
+
+    Parameters
+        radius: float
+
+    Methods:
+        area()
+        perimeter()
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * abs(self.radius) ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * abs(self.radius)
+
+
+class Rectangle(Shape):
+    """
+    New class Rectangle, a subclass of Shape
+
+    Parameters:
+        width
+        height
+    Methods:
+        area()
+        perimeter()
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return abs(self.width) * abs(self.height)
+
+    def perimeter(self):
+        return 2 * (abs(self.width) + abs(self.height))
+
+
+def shape_info(shape: Shape):
+    """
+    Prints the area and perimeter of the given shape
+    """
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
