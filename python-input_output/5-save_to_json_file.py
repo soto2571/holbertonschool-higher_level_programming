d1d92d190a32a06eb8549397dfb14266b8788734
soto2571 <sebastian.soto1649@gmail.com>
2024-05-27 16:20:42 -0400

    pushing 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Writing a function that writes an object to a text file using JSON
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    This function writes an object to a text
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
