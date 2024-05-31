19439fa15cf0e97fad60272f85a7aafa48d7eeba
soto2571 <sebastian.soto1649@gmail.com>
2024-05-31 19:28:27 -0400

    jffjl

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,10 @@
-import http.server
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import socketserver
 
-PORT = 8000
 
-class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -15,12 +15,8 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            response = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -30,9 +26,15 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"Endpoint not fount")
 
-# Start the server
-with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-    print(f"Serving on port {PORT}")
-    httpd.serve_forever()
+
+def run():
+    server_address = ('', 8000)
+    http = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print('Starting server...')
+    http.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
