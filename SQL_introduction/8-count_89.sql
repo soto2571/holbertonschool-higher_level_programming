f1b6126b38968b6ab9c31585978afae0fade58bd
soto2571 <sebastian.soto1649@gmail.com>
2024-06-17 20:34:03 -0400

    fjklf

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,12 +1,2 @@
--- Script: 8-count_89.sql
-
--- Purpose: Count the number of records with id = 89 in the first_table of hbtn_0c_0 database
-
--- Author: [Your Name]
-
--- Date: [Date]
-
-SELECT COUNT(*) AS count_89
-FROM hbtn_0c_0.first_table
-WHERE id = 89;
-
+-- displays the num of records with id = 89 in table first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
