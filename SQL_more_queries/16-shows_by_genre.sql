20aa6a432adf7a869c508104f5c81639ac728cfe
soto2571 <sebastian.soto1649@gmail.com>
2024-06-18 19:11:03 -0400

    fjksf

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
--- list all shows and all genres linked to that show from datbase htb...tvshow
+-- list all shows and all genres linked
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
