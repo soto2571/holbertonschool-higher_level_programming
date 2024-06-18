206ed71b736c671833f9dd2cf92b671073abd7f9
soto2571 <sebastian.soto1649@gmail.com>
2024-06-17 20:38:12 -0400

    ffksljfs

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,16 +1,5 @@
--- Script: 11-best_score.sql
-
--- Purpose: List all records with a score >= 10 in the table second_table of the hbtn_0c_0 database
-
--- Author: [Your Name]
-
--- Date: [Date]
-
-USE hbtn_0c_0;
-
--- Query to select records with score >= 10 from second_table, ordered by score descending
+-- this writes the list of all the records of a score >= 10 in table second...
 SELECT score, name
 FROM second_table
 WHERE score >= 10
-ORDER BY score DESC;
-
+ORDER BY score DESC
