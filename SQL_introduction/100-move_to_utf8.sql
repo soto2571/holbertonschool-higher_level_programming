b68589b8f85ebc966d9ae02c6030dfc62a84e02a
soto2571 <sebastian.soto1649@gmail.com>
2024-06-17 20:46:40 -0400

    skffj

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,5 @@ USE hbtn_0c_0;
 -- Convert the table first_table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field name in first_table to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the field name in first_table to UTF8 without explicitly mentioning CHARACTER SET
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
